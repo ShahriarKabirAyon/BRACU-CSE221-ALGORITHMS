69de43ec1a63260e873a7ad9f868469da41dfd98
SHAHRIAR KABIR AYON <shahriar.kabir.ayon@g.bracu.ac.bd>
2025-09-27 01:41:07 +0600

    Delete CSE221 Lab Problems and Solutions/LAB 01 directory

--- CSE221 Lab Problems and Solutions/LAB 01/Odd or Even.py
+++
@@ -1,9 +0,0 @@
-T=int(input())
-if 1<=T and T<=100:
-    for i in range (T):
-        N=int(input())
-        if -10**5<=N and N<=10**5:
-            if N%2==0:
-                print(f"{N} is an Even number.")
-            else:
-                print(f"{N} is an Odd number.")
